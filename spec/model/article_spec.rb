7733c336b17480592a5662670231e87a690c7173
Dmitriy Angelovskiy <dmitriy.angelovskiy@gmail.com>
2017-08-28 17:48:21 +0300

    Update Article spec

--- spec/model/article_spec.rb
+++ spec/model/article_spec.rb
@@ -9,4 +9,15 @@ describe Article, type: :model do
   describe 'associations' do
     it { should have_many :comments }
   end
+
+  describe '#subject' do
+    it 'returns the article title' do
+      # создаем объект article хитрым способом
+      article = create(:article, title: 'Lorem Ipsum')
+
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
+
 end
